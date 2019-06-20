161e520d9e6315cdb0b3b0ce37e66cc58abaab17
Roybal_A <Roybal.usmc@gmail.com>
2019-06-20 16:01:27 -0700

    pseudo code and style guidelines

---
+++ chapter-9-programming-styles/style-guidelines.js
@@ -0,0 +1,17 @@
+//naming comventions
+
+//variable names need to be descriptive and specific
+//names should be written in camelCase
+//functions should use the same camelCase rule
+
+//curly brace placement
+//if(x) {
+
+//} else {
+
+//}
+//this is acceptable but above should take precedence
+//if(x)
+//{
+
+//}
---
+++ chapter-9-programming-styles/writing-pseudo-code.js
@@ -0,0 +1,32 @@
+//pseudo code
+//should be written before programming
+
+/* ask user for email address
+
+  if email address matches accepted pattern
+    add them to email list
+  else
+    show error message
+*/
+
+/* IF balance < 0
+    display "balance is negative!" message
+  ELSE
+    display "balance OK" message
+  END IF
+*/
+
+//getting total number
+/*  set total to zero
+    get list of numbers
+
+    loop through each number in the list
+      add each number to total
+    end loop
+
+    if number more than zero
+      print "it's positive" message
+    else
+      print "it's zero or less" message
+    end if
+*/
